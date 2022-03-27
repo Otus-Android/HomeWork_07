cb45bc823a714baafa0d21ee1bfddd6d73a3406a
juliakardanova <j.kardanova@soax.com>
2022-03-27 19:07:03 +0300

    Add custom GraphView

--- app/src/main/java/otus/homework/customview/MainActivity.kt
+++ app/src/main/java/otus/homework/customview/MainActivity.kt
@@ -4,19 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.google.gson.Gson
+import otus.homework.customview.graph.GraphView
 import otus.homework.customview.paichart.PieChartView
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var pieChart: PieChartView
+    private lateinit var graph: GraphView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         pieChart = findViewById(R.id.pie_chart_view)
+        graph = findViewById(R.id.graph_view)
 
-        savedInstanceState ?: setupPieChart()
+        setupPieChart()
     }
 
     private fun setupPieChart() {
@@ -26,16 +29,15 @@ class MainActivity : AppCompatActivity() {
         val payments = Gson().fromJson(payload, Array<Payment>::class.java).toList()
         val total = payments.sumBy { it.amount }
 
-        val paymentsByCategory = payments.groupingBy { it.category }
-            .aggregateTo(mutableMapOf()) { _, accumulator: Int?, el, first ->
-                if (first) el.amount else accumulator!!.plus(el.amount)
-            }.mapTo(mutableListOf()) {
-                Pair(it.key, it.value * 100f/ total)
-            }
+        val byCategory = payments.groupBy { it.category }
 
-        pieChart.setData(total, paymentsByCategory)
+        val pieChartData = byCategory.mapTo(mutableListOf()) { category ->
+            Pair(category.key, category.value.sumOf { it.amount } * 100f/ total )
+        }
+
+        pieChart.setData(total, pieChartData)
         pieChart.onClickListener = { category ->
-            Log.d(this::class.simpleName, "onPieChartCategoryClicked: $category")
+            byCategory[category]?.let { list -> graph.setData(list.associate { it.date to it.amount }) }
         }
     }
 }
