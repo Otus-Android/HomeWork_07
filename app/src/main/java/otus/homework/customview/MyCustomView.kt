db1642dd38dd313956be4e08106cd2e4b43391b5
ilya <lex7555@yandex.ru>
2022-07-01 23:19:16 +0300

    Накидал тестовую версию.

--- app/src/main/java/otus/homework/customview/MyCustomView.kt
+++ app/src/main/java/otus/homework/customview/MyCustomView.kt
@@ -1,17 +1,18 @@
 package otus.homework.customview
 
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
-import android.graphics.PathMeasure
-import android.graphics.RectF
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import androidx.annotation.ColorInt
 import kotlin.math.atan2
+import kotlin.math.min
 import kotlin.random.Random
 
 
@@ -20,9 +21,6 @@ class MyCustomView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
-
-    var defaultWidth = 600
-    var defaultHeight = 600
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         Log.i("myDebug", "onMeasure")
@@ -47,138 +45,176 @@ class MyCustomView @JvmOverloads constructor(
             MeasureSpec.UNSPECIFIED -> {
                 measureSpecWidth
             }
-            else -> throw IllegalStateException("Zopa")
+            else -> throw IllegalStateException("Incorrect Width")
         }
 
         val currentHeight: Int = when (measureSpecHeightMode) {
             MeasureSpec.EXACTLY -> {
-                measureSpecWidth
+                measureSpecHeight
             }
             MeasureSpec.AT_MOST -> {
-                measureSpecWidth
+                measureSpecHeight
             }
             MeasureSpec.UNSPECIFIED -> {
-                measureSpecWidth
+                measureSpecHeight
             }
-            else -> throw IllegalStateException("Zopa")
+            else -> throw IllegalStateException("Incorrect Height")
         }
 
-        setMeasuredDimension(currentWidth, currentHeight)
+        val minSideValue = min(currentWidth, currentHeight)
+
+        setMeasuredDimension(minSideValue, minSideValue)
     }
 
-    val linePaint = Paint().apply {
+    private val maxSegmentSize = 50f.toPx
+
+    private val smallRadius = 100f.toPx
+
+    private val x0
+        get() = min(measuredHeight, measuredWidth).toFloat() / 2
+
+    private val y0
+        get() = min(measuredHeight, measuredWidth).toFloat() / 2
+
+    private val segment1 = Segment(
+        startAngel = 0f,
+        endAngel = 30f,
+        segmentWidth = 10f.toPx
+    )
+
+    private val segment2 = Segment(
+        startAngel = 35f,
+        endAngel = 65f,
+        segmentWidth = 25f.toPx
+    )
+
+    private val segment3 = Segment(
+        startAngel = 75f,
+        endAngel = 135f,
+        segmentWidth = 20f.toPx
+    )
+
+    private val segment4 = Segment(
+        startAngel = 140f,
+        endAngel = 355f,
+        segmentWidth = 50f.toPx
+    )
+
+    private val arrayOfSegments = arrayListOf<Segment>(segment1, segment2, segment3, segment4)
+
+    private val linePaint = Paint().apply {
         color = Color.GREEN
         style = Paint.Style.STROKE
         flags = Paint.ANTI_ALIAS_FLAG
         strokeWidth = 10f
     }
-
-    val rectPaint = Paint().apply {
-        color = Color.BLUE
-        style = Paint.Style.STROKE
-        flags = Paint.ANTI_ALIAS_FLAG
-        strokeWidth = 10f
-    }
-
-    val paintStroke = 200f
-    val paint1 = Paint().apply {
-        color = Color.BLACK
-        style = Paint.Style.STROKE
-        flags = Paint.ANTI_ALIAS_FLAG
-        strokeWidth = paintStroke
-    }
-
-    val paint2 = Paint().apply {
-        color = Color.GREEN
-        style = Paint.Style.STROKE
-        flags = Paint.ANTI_ALIAS_FLAG
-        strokeWidth = paintStroke
-    }
-
-    val paint3 = Paint().apply {
-        color = Color.BLUE
-        style = Paint.Style.STROKE
-        flags = Paint.ANTI_ALIAS_FLAG
-        strokeWidth = paintStroke
-    }
-    val path1 = Path()
-    val path2 = Path()
-    val path3 = Path()
-    val path4 = Path()
-    val rect1 = RectF()
-
-    val pathMeasure = PathMeasure()
-
-    var centerX = 0f
-    var centerY = 0f
-
-    private val bigRadius 
-        get() = measuredWidth.toFloat() / 2 - paintStroke / 2
-
-    private val smallRadius
-        get() = measuredWidth.toFloat() / 2 - paintStroke - paintStroke / 2
-
-    private val xOffset
-        get() = measuredWidth.toFloat() / 2
-
-    private val yOffset
-        get() = measuredHeight.toFloat() / 2
 
     override fun onDraw(canvas: Canvas) {
         canvas.drawRGB(255, 0, 0)
 
-        rect1.set(
-            0f + paintStroke,
-            0f + paintStroke,
-            measuredWidth.toFloat() - paintStroke,
-            measuredHeight.toFloat() - paintStroke
-        )
-        canvas.drawRect(rect1, rectPaint)
-
-        centerX = rect1.centerX()
-        centerY = rect1.centerY()
-
-        canvas.drawLine(
-            centerX,
-            centerY,
-            centerX,
-            centerY - bigRadius,
-            linePaint
-        )
-
-        canvas.drawLine(
-            centerX,
-            centerY,
-            centerX + smallRadius,
-            centerY,
-            linePaint
-        )
-
-        path1.addArc(rect1, 0f, 70f)
-        path2.addArc(rect1, 75f, 70f)
-        path3.addArc(rect1, 150f, 100f)
-
-        canvas.drawPath(path1, paint1)
-        canvas.drawPath(path2, paint2)
-        canvas.drawPath(path3, paint3)
+        arrayOfSegments.forEach {
+            it.onDraw(
+                x0 = x0,
+                y0 = y0,
+                smallRadius = smallRadius,
+                paint = linePaint,
+                canvas = canvas
+            )
+        }
     }
 
-    private fun isTouchInSegment(
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                Log.i("myDebug", "onTouchEvent: x -> ${event.x}")
+                Log.i("myDebug", "onTouchEvent: y -> ${event.y}")
+
+                arrayOfSegments.forEach { segment ->
+                    val isTouchInSegment = segment.isTouchInSegment(
+                        xPoint = event.x,
+                        yPoint = event.y,
+                        bigRadius = smallRadius + segment.segmentWidth,
+                        smallRadius = smallRadius,
+                        x0 = x0,
+                        y0 = y0
+                    )
+
+                    if (isTouchInSegment) {
+                        val touchPointAngel = segment.isTouchInSegment1(event.x, event.y, x0, y0)
+                        if (touchPointAngel) {
+                            segment.color = generateRandomColor()
+                            invalidate()
+                        }
+                    }
+                }
+            }
+            else -> {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+class Segment(
+    private val startAngel: Float,
+    private val endAngel: Float,
+    val segmentWidth: Float,
+    @ColorInt var color: Int = generateRandomColor()
+) {
+    private val path = Path()
+
+    fun onDraw(
+        x0: Float,
+        y0: Float,
+        smallRadius: Float,
+        paint: Paint,
+        canvas: Canvas
+    ) {
+        path.reset()
+        path.addArc(
+            x0 - smallRadius - segmentWidth / 2,
+            y0 - smallRadius - segmentWidth / 2,
+            x0 + smallRadius + segmentWidth / 2,
+            y0 + smallRadius + segmentWidth / 2,
+            startAngel, endAngel - startAngel
+        )
+
+        paint.color = color
+        paint.strokeWidth = segmentWidth
+
+        canvas.drawPath(path, paint)
+    }
+
+    fun isTouchInSegment1(
+        xPoint: Float,
+        yPoint: Float,
+        x0Point: Float,
+        y0Point: Float
+    ): Boolean {
+        val angel = getAngleFromTouch(
+            xPoint = xPoint,
+            yPoint = yPoint,
+            x0Point = x0Point,
+            y0Point = y0Point
+        )
+
+        return angel in startAngel..endAngel
+    }
+
+    fun isTouchInSegment(
         xPoint: Float,
         yPoint: Float,
         bigRadius: Float,
         smallRadius: Float,
-        xOffset: Float,
-        yOffset: Float
-
+        x0: Float,
+        y0: Float
     ): Boolean {
-        val dX = xPoint - xOffset
-        val dY = yPoint - yOffset
-        Log.i("myDebug", "inCorrectRange: dX -> $dX")
-        Log.i("myDebug", "inCorrectRange: dY -> $dY")
+        val dX = xPoint - x0
+        val dY = yPoint - y0
 
-        return (dX * dX + dY * dY) < bigRadius * bigRadius &&
-                (dX * dX + dY * dY) > smallRadius * smallRadius
+        return (dX * dX + dY * dY) < bigRadius * bigRadius
+                && (dX * dX + dY * dY) > smallRadius * smallRadius
     }
 
     private fun getAngleFromTouch(
@@ -195,48 +231,21 @@ class MyCustomView @JvmOverloads constructor(
 
         return angelInDegrees.toFloat() + 180
     }
-
-    private fun Paint.setRandomRGB() {
-        this.setARGB(
-            255,
-            Random.nextInt(256),
-            Random.nextInt(256),
-            Random.nextInt(256)
-        )
-    }
-
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        when (event?.action) {
-            MotionEvent.ACTION_DOWN -> {
-                Log.i("myDebug", "onTouchEvent: x -> ${event.x}")
-                Log.i("myDebug", "onTouchEvent: y -> ${event.y}")
-                val inCorrectPlaceTouch = isTouchInSegment(
-                    xPoint = event.x,
-                    yPoint = event.y,
-                    bigRadius = bigRadius,
-                    smallRadius = smallRadius,
-                    xOffset = xOffset,
-                    yOffset = yOffset
-                )
-
-                val touchPointAngel = getAngleFromTouch(event.x, event.y, xOffset, yOffset)
-
-                when {
-                    touchPointAngel in 0.0..70.0 && inCorrectPlaceTouch -> {
-                        paint1.setRandomRGB()
-                        invalidate()
-                    }
-                    touchPointAngel in 75.0..145.0 && inCorrectPlaceTouch -> {
-                        paint2.setRandomRGB()
-                        invalidate()
-                    }
-                    touchPointAngel in 150.0..250.0 && inCorrectPlaceTouch-> {
-                        paint3.setRandomRGB()
-                        invalidate()
-                    }
-                }
-            }
-        }
-        return true
-    }
 }
+
+fun generateRandomColor(): Int =
+    Color.argb(
+        255,
+        Random.nextInt(256),
+        Random.nextInt(256),
+        Random.nextInt(256)
+    )
+
+val Float.toPx get() = this * Resources.getSystem().displayMetrics.density
+
+val Float.toDp get() = this / Resources.getSystem().displayMetrics.density
+
+
+val Int.toPx get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+val Int.toDp get() = (this / Resources.getSystem().displayMetrics.density).toInt()
