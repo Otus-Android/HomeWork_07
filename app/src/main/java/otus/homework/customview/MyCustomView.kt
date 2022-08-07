07b8823feb377f0d9339e8569762c76c1faf0a13
ilya <lex7555@yandex.ru>
2022-08-07 23:56:04 +0300

    Рефакторинг.

--- app/src/main/java/otus/homework/customview/MyCustomView.kt
+++ app/src/main/java/otus/homework/customview/MyCustomView.kt
@@ -2,25 +2,15 @@ package otus.homework.customview
 
 import android.content.Context
 import android.content.res.Resources
-import android.graphics.BlurMaskFilter
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.Paint.FontMetricsInt
 import android.graphics.Path
-import android.graphics.Rect
-import android.graphics.RectF
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import androidx.annotation.ColorInt
-import java.math.RoundingMode
-import java.text.DecimalFormat
-import kotlin.math.atan2
-import kotlin.math.cos
 import kotlin.math.min
-import kotlin.math.sin
 import kotlin.random.Random
 
 
@@ -30,18 +20,61 @@ class MyCustomView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
+    private val arrayOfSegments = mutableListOf<Segment>()
+    private var amountOfAllCategoriesText = ""
+    private var currentMonthText =
+        "In september" // Специально оставил тут без выноса в ресурсы или через получение через json
+
+    private val maxSegmentSize
+        get() = min(measuredWidth, measuredHeight) / 6f
+
+    private val smallRadius
+        get() = min(measuredWidth, measuredHeight) / 5f
+
+    private val x0
+        get() = measuredWidth / 2f
+
+    private val y0
+        get() = measuredHeight / 2f
+
+    private val segmentsPaint = Paint().apply {
+        color = Color.GREEN
+        style = Paint.Style.STROKE
+        flags = Paint.ANTI_ALIAS_FLAG
+    }
+
+    private val circleColor = Color.CYAN
+    private val circlePath = Path()
+    private val circlePaint = Paint().apply {
+        color = circleColor
+        style = Paint.Style.FILL
+        flags = Paint.ANTI_ALIAS_FLAG
+    }
+
+    private val amountTextPaint = Paint().apply {
+        color = Color.GRAY
+        style = Paint.Style.FILL
+        flags = Paint.ANTI_ALIAS_FLAG
+        textSize = 24f.toPx
+        isFakeBoldText = true
+        textAlign = Paint.Align.CENTER
+    }
+
+    private val monthTextPaint = Paint().apply {
+        color = Color.LTGRAY
+        style = Paint.Style.FILL
+        flags = Paint.ANTI_ALIAS_FLAG
+        textSize = 14f.toPx
+        isFakeBoldText = true
+        textAlign = Paint.Align.CENTER
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        Log.i("myDebug", "onMeasure")
 
         val measureSpecWidthMode = MeasureSpec.getMode(widthMeasureSpec)
         val measureSpecWidth = MeasureSpec.getSize(widthMeasureSpec)
         val measureSpecHeightMode = MeasureSpec.getMode(heightMeasureSpec)
         val measureSpecHeight = MeasureSpec.getSize(heightMeasureSpec)
-
-        Log.i("myDebug", "onMeasure: measureSpecWidthMode -> $measureSpecWidthMode")
-        Log.i("myDebug", "onMeasure: measureSpecWidth -> $measureSpecWidth")
-        Log.i("myDebug", "onMeasure: measureSpecHeightMode -> $measureSpecHeightMode")
-        Log.i("myDebug", "onMeasure: measureSpecHeight -> $measureSpecHeight")
 
         val currentWidth: Int = when (measureSpecWidthMode) {
             MeasureSpec.EXACTLY -> {
@@ -51,7 +84,7 @@ class MyCustomView @JvmOverloads constructor(
                 measureSpecWidth
             }
             MeasureSpec.UNSPECIFIED -> {
-                measureSpecWidth
+                min(measureSpecWidth, measureSpecHeight)
             }
             else -> throw IllegalStateException("Incorrect Width")
         }
@@ -64,94 +97,50 @@ class MyCustomView @JvmOverloads constructor(
                 measureSpecHeight
             }
             MeasureSpec.UNSPECIFIED -> {
-                measureSpecHeight
+                min(measureSpecWidth, measureSpecHeight)
             }
             else -> throw IllegalStateException("Incorrect Height")
         }
 
-        val minSideValue = min(currentWidth, currentHeight)
-
-        setMeasuredDimension(minSideValue, minSideValue)
-    }
-
-    private val maxSegmentSize = 80f.toPx
-
-    private val smallRadius = 80f.toPx
-
-    private val x0
-        get() = min(measuredHeight, measuredWidth).toFloat() / 2
-
-    private val y0
-        get() = min(measuredHeight, measuredWidth).toFloat() / 2
-
-    private val arrayOfSegments = mutableListOf<Segment>()
-
-    private val linePaint = Paint().apply {
-        color = Color.GREEN
-        style = Paint.Style.STROKE
-        flags = Paint.ANTI_ALIAS_FLAG
-        strokeWidth = 10f
-    }
-
-    private val circlePath = Path()
-
-    private val blurFilter = BlurMaskFilter(16f, BlurMaskFilter.Blur.SOLID)
-
-    private val circleColor = Color.argb(230, 255, 255, 255)
-
-    private val circleTextPaint = Paint().apply {
-        color = Color.BLACK
-        style = Paint.Style.FILL
-        flags = Paint.ANTI_ALIAS_FLAG
-        textSize = 40f
-        textAlign = Paint.Align.CENTER
-    }
-
-    private val circlePaint = Paint().apply {
-        color = circleColor
-        style = Paint.Style.FILL
-        flags = Paint.ANTI_ALIAS_FLAG
-        strokeWidth = 10f
-        maskFilter = blurFilter
+        setMeasuredDimension(currentWidth, currentHeight)
     }
 
     override fun onDraw(canvas: Canvas) {
-        canvas.drawRGB(255, 0, 0)
-
         arrayOfSegments.forEach {
             it.onDraw(
                 x0 = x0,
                 y0 = y0,
+                maxSegmentWidth = maxSegmentSize,
                 smallRadius = smallRadius,
-                paint = linePaint,
+                paint = segmentsPaint,
                 canvas = canvas
             )
         }
 
-
-//        circlePath.addCircle(x0, y0, smallRadius - 5f.toPx, Path.Direction.CW)
-//        canvas.drawPath(circlePath, circlePaint)
-//        canvas.drawText("20199$", x0, y0, circleTextPaint)
+        drawCentralCircle(canvas)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         when (event?.action) {
             MotionEvent.ACTION_DOWN -> {
-                Log.i("myDebug", "onTouchEvent: x -> ${event.x}")
-                Log.i("myDebug", "onTouchEvent: y -> ${event.y}")
 
                 arrayOfSegments.forEach { segment ->
-                    val isTouchInSegment = segment.isTouchInSegment(
+                    val isTouchInSegment = segment.isTouchInSegmentRange(
                         xPoint = event.x,
                         yPoint = event.y,
-                        bigRadius = smallRadius + segment.segmentWidth,
                         smallRadius = smallRadius,
                         x0 = x0,
                         y0 = y0
                     )
 
                     if (isTouchInSegment) {
-                        val touchPointAngel = segment.isTouchInSegment1(event.x, event.y, x0, y0)
+                        val touchPointAngel =
+                            segment.isTouchInCurrentSegment(
+                                xPoint = event.x,
+                                yPoint = event.y,
+                                x0Point = x0,
+                                y0Point = y0
+                            )
                         if (touchPointAngel) {
                             segment.color = generateRandomColor()
                             invalidate()
@@ -167,205 +156,50 @@ class MyCustomView @JvmOverloads constructor(
     }
 
     fun setData(dataEntity: SegmentsDataEntity) {
-        Log.i("myDebug", "onCreate: dataEntity -> $dataEntity")
-
-        val sumOfAllCategories = dataEntity.data.sumOf { it.amount }
-        val groupOfCategories = dataEntity.data.groupBy { it.category }
-        val countOfDelimiters = groupOfCategories.size
         val sizeOfDelimitersInPercents = 1f
         val allSegmentSize = 100
-        Log.i("myDebug", "setData: sumOfAllCategories -> $sumOfAllCategories")
-        Log.i("myDebug", "setData: groupOfCategories -> $groupOfCategories")
-        Log.i("myDebug", "setData: countOfGroups -> $countOfDelimiters")
-        Log.i("myDebug", "setData: sizeOfDelimitersInPercents -> $sizeOfDelimitersInPercents")
-        Log.i("myDebug", "setData: allSegmentSize -> $allSegmentSize")
-        val sumOfEachCategories = groupOfCategories.mapValues {
+        val degreesInCircle = 360f
+
+        val amountOfAllCategories = dataEntity.data.sumOf { it.amount }
+        amountOfAllCategoriesText = "$ $amountOfAllCategories"
+
+        val groupOfCategories = dataEntity.data.groupBy { it.category }
+
+        val countOfDelimiters = groupOfCategories.size
+
+        val amountOfEachCategories = groupOfCategories.mapValues {
             it.value.sumOf { items -> items.amount }
         }
-        val eachCategoriesInPercents =
-            sumOfEachCategories.mapValues { it.value * allSegmentSize / 100f * 1 / sumOfAllCategories }
-        val sumOfCategories2 = eachCategoriesInPercents.map { it.value }.sum()
 
-        Log.i("myDebug", "setData: sumOfEachCategories -> $sumOfEachCategories")
-        Log.i("myDebug", "setData: eachCategoriesInPercents -> $eachCategoriesInPercents")
-        Log.i("myDebug", "setData: sumOfCategories2 -> $sumOfCategories2")
+        val eachCategoriesInPercents = amountOfEachCategories
+            .mapValues { it.value * allSegmentSize / 100f / amountOfAllCategories }
 
-
-        val eachCategoriesInDegrees = eachCategoriesInPercents.mapValues { it.value * 360 }
-        val eachCategoriesInDegreesSum = eachCategoriesInPercents.map { it.value }.sum()
-        Log.i("myDebug", "setData: eachCategoriesInDegrees -> $eachCategoriesInDegrees")
-        Log.i("myDebug", "setData: eachCategoriesInDegreesSum -> $eachCategoriesInDegreesSum")
+        val eachCategoriesInDegrees = eachCategoriesInPercents
+            .mapValues { it.value * degreesInCircle }
 
         eachCategoriesInDegrees.map { it.value }.fold(0f) { acc, fl ->
-            Log.i("myDebug", "setData: acc -> $acc, fl -> $fl")
             val result = acc + fl
 
             val segmentUIEntity = Segment(
                 startAngel = acc,
-                endAngel = result - 3.6f * sizeOfDelimitersInPercents,
-                segmentWidth = 0f.toPx
+                endAngel = result - (degreesInCircle * sizeOfDelimitersInPercents / 100)
             )
 
-            Log.i("myDebug", "setData: segmentUIEntity -> $segmentUIEntity")
             arrayOfSegments.add(segmentUIEntity)
 
             result
         }
 
         eachCategoriesInPercents.map { it.value }.forEachIndexed { index, fl ->
-            arrayOfSegments[index].segmentWidth = maxSegmentSize * (fl + 0.4f)
             arrayOfSegments[index].percents = fl * 100f
         }
-
-    }
-}
-
-data class Segment(
-    private val startAngel: Float,
-    private val endAngel: Float,
-    var segmentWidth: Float,
-    @ColorInt var color: Int = generateRandomColor(),
-    var percents: Float = 0f
-) {
-    private val path = Path()
-    private val blurFilter = BlurMaskFilter(8f.toDp, BlurMaskFilter.Blur.SOLID)
-
-    private val textPaint = Paint().apply {
-        color = Color.LTGRAY
-        style = Paint.Style.FILL
-        flags = Paint.ANTI_ALIAS_FLAG
-        textSize = 20f
-        textAlign = Paint.Align.CENTER
     }
 
-    private val rectPaint = Paint().apply {
-        color = Color.LTGRAY
-        style = Paint.Style.STROKE
-        flags = Paint.ANTI_ALIAS_FLAG
-        strokeWidth = 2f
-        maskFilter = blurFilter
-    }
-
-    private val df = DecimalFormat("#.##").apply {
-        roundingMode = RoundingMode.DOWN
-    }
-
-    fun onDraw(
-        x0: Float,
-        y0: Float,
-        smallRadius: Float,
-        paint: Paint,
-        canvas: Canvas
-    ) {
-        path.reset()
-        path.addArc(
-            x0 - smallRadius - segmentWidth / 2,
-            y0 - smallRadius - segmentWidth / 2,
-            x0 + smallRadius + segmentWidth / 2,
-            y0 + smallRadius + segmentWidth / 2,
-            startAngel,
-            endAngel - startAngel
-        )
-
-        paint.color = color
-        paint.strokeWidth = segmentWidth
-        paint.maskFilter = blurFilter
-
-        canvas.drawPath(path, paint)
-        calculatePoint(
-            canvas = canvas,
-            smallRadius = smallRadius,
-            x0 = x0,
-            y0 = y0,
-            paint = paint,
-            offset = 50f
-        )
-    }
-
-    private val rectRoundOfText = RectF()
-
-    private fun calculatePoint(
-        canvas: Canvas,
-        smallRadius: Float,
-        x0: Float,
-        y0: Float,
-        paint: Paint,
-        offset: Float
-    ) {
-        val centerOfSegment = startAngel + (endAngel - startAngel) / 2
-        val alfa = centerOfSegment * Math.PI / 180
-
-        canvas.drawCircle(x0, y0, smallRadius, textPaint)
-
-
-        val x = x0 + (smallRadius + segmentWidth + offset) * cos(alfa).toFloat()
-        val y = y0 + (smallRadius + segmentWidth + offset) * sin(alfa).toFloat()
-
-
-        val text = "${df.format(percents)}%"
-        val measureText = textPaint.measureText(text)
-        val textSize = textPaint.textSize
-
-        val offset = 8f
-        val leftRect = x - measureText / 2 - offset
-        val topRect = y - textSize / 2 - offset
-        val rightRect = x + measureText / 2 + offset
-        val bottomRect = y + textSize / 2 + offset
-
-        rectRoundOfText.set(leftRect, topRect, rightRect, bottomRect)
-
-        val fontMetricsInt: FontMetricsInt = textPaint.fontMetricsInt
-        canvas.drawRoundRect(rectRoundOfText, 8f, 8f, rectPaint)
-
-        val centYText: Float =
-            rectRoundOfText.centerY() - (fontMetricsInt.top + fontMetricsInt.bottom) / 2
-        canvas.drawText(text, x, centYText, textPaint)
-    }
-
-    fun isTouchInSegment1(
-        xPoint: Float,
-        yPoint: Float,
-        x0Point: Float,
-        y0Point: Float
-    ): Boolean {
-        val angel = getAngleFromTouch(
-            xPoint = xPoint,
-            yPoint = yPoint,
-            x0Point = x0Point,
-            y0Point = y0Point
-        )
-
-        return angel in startAngel..endAngel
-    }
-
-    fun isTouchInSegment(
-        xPoint: Float,
-        yPoint: Float,
-        bigRadius: Float,
-        smallRadius: Float,
-        x0: Float,
-        y0: Float
-    ): Boolean {
-        val dX = xPoint - x0
-        val dY = yPoint - y0
-
-        return (dX * dX + dY * dY) < bigRadius * bigRadius
-                && (dX * dX + dY * dY) > smallRadius * smallRadius
-    }
-
-    private fun getAngleFromTouch(
-        xPoint: Float,
-        yPoint: Float,
-        x0Point: Float,
-        y0Point: Float
-    ): Float {
-        val dX = (x0Point - xPoint).toDouble()
-        val dY = (y0Point - yPoint).toDouble()
-
-        val angelInRadians = atan2(dY, dX)
-        val angelInDegrees = angelInRadians * 180 / Math.PI
-
-        return angelInDegrees.toFloat() + 180
+    private fun drawCentralCircle(canvas: Canvas) {
+        circlePath.addCircle(x0, y0, smallRadius - 5f.toPx, Path.Direction.CW)
+        canvas.drawPath(circlePath, circlePaint)
+        canvas.drawText(amountOfAllCategoriesText, x0, y0, amountTextPaint)
+        canvas.drawText(currentMonthText, x0, y0 + 18f.toPx, monthTextPaint)
     }
 }
 
@@ -380,8 +214,3 @@ fun generateRandomColor(): Int =
 val Float.toPx get() = this * Resources.getSystem().displayMetrics.density
 
 val Float.toDp get() = this / Resources.getSystem().displayMetrics.density
-
-
-val Int.toPx get() = (this * Resources.getSystem().displayMetrics.density).toInt()
-
-val Int.toDp get() = (this / Resources.getSystem().displayMetrics.density).toInt()
---
+++ app/src/main/java/otus/homework/customview/Segment.kt
@@ -0,0 +1,180 @@
+package otus.homework.customview
+
+import android.graphics.BlurMaskFilter
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.RectF
+import androidx.annotation.ColorInt
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sin
+
+data class Segment(
+    private val startAngel: Float,
+    private val endAngel: Float,
+    @ColorInt var color: Int = generateRandomColor(),
+    var percents: Float = 0f
+) {
+    var segmentWidth: Float = 0f
+        get() = field * (percents / 100f + 0.4f)
+
+    private val df = DecimalFormat("#.##").apply {
+        roundingMode = RoundingMode.DOWN
+    }
+
+    private val offsetOfPercentTextRect = 3f.toPx
+    private val offsetOfPercentText = 24f.toPx
+    private val roundRectOfPercentText = RectF()
+    private val valueOfPercentText by lazy { "${df.format(percents)} %" }
+
+    private val arcPath = Path()
+
+    private val blurFilter = BlurMaskFilter(3f.toPx, BlurMaskFilter.Blur.SOLID)
+
+    private val textPaint = Paint().apply {
+        color = Color.LTGRAY
+        style = Paint.Style.FILL
+        flags = Paint.ANTI_ALIAS_FLAG
+        textSize = 10f.toPx
+        textAlign = Paint.Align.CENTER
+    }
+
+    private val rectPaint = Paint().apply {
+        color = Color.LTGRAY
+        style = Paint.Style.STROKE
+        flags = Paint.ANTI_ALIAS_FLAG
+        strokeWidth = 1f.toPx
+        maskFilter = blurFilter
+    }
+
+    fun onDraw(
+        x0: Float,
+        y0: Float,
+        maxSegmentWidth: Float,
+        smallRadius: Float,
+        paint: Paint,
+        canvas: Canvas
+    ) {
+        segmentWidth = maxSegmentWidth
+
+        drawSegment(x0, smallRadius, y0, paint, canvas)
+        drawPercentText(
+            canvas = canvas,
+            segmentWidth = segmentWidth,
+            smallRadius = smallRadius,
+            x0 = x0,
+            y0 = y0,
+            paint = paint,
+            offset = offsetOfPercentText
+        )
+    }
+
+    private fun drawSegment(
+        x0: Float,
+        smallRadius: Float,
+        y0: Float,
+        paint: Paint,
+        canvas: Canvas
+    ) {
+        arcPath.reset()
+        arcPath.addArc(
+            x0 - smallRadius - segmentWidth / 2,
+            y0 - smallRadius - segmentWidth / 2,
+            x0 + smallRadius + segmentWidth / 2,
+            y0 + smallRadius + segmentWidth / 2,
+            startAngel,
+            endAngel - startAngel
+        )
+
+        paint.color = color
+        paint.strokeWidth = segmentWidth
+        paint.maskFilter = blurFilter
+
+        canvas.drawPath(arcPath, paint)
+    }
+
+    private fun drawPercentText(
+        canvas: Canvas,
+        segmentWidth: Float,
+        smallRadius: Float,
+        x0: Float,
+        y0: Float,
+        paint: Paint,
+        offset: Float
+    ) {
+        val centerOfSegment = startAngel + (endAngel - startAngel) / 2
+        val alfa = centerOfSegment * Math.PI / 180
+
+        val x = x0 + (smallRadius + segmentWidth + offset) * cos(alfa).toFloat()
+        val y = y0 + (smallRadius + segmentWidth + offset) * sin(alfa).toFloat()
+
+        val text = valueOfPercentText
+        val measureText = textPaint.measureText(text)
+        val textSize = textPaint.textSize
+
+        val leftRect = x - measureText / 2 - offsetOfPercentTextRect
+        val topRect = y - textSize / 2 - offsetOfPercentTextRect
+        val rightRect = x + measureText / 2 + offsetOfPercentTextRect
+        val bottomRect = y + textSize / 2 + offsetOfPercentTextRect
+
+        roundRectOfPercentText.set(leftRect, topRect, rightRect, bottomRect)
+
+        val fontMetricsInt: Paint.FontMetricsInt = textPaint.fontMetricsInt
+        canvas.drawRoundRect(roundRectOfPercentText, 8f, 8f, rectPaint)
+
+        val centerYText: Float =
+            roundRectOfPercentText.centerY() - (fontMetricsInt.top + fontMetricsInt.bottom) / 2
+        canvas.drawText(text, x, centerYText, textPaint)
+    }
+
+    fun isTouchInCurrentSegment(
+        xPoint: Float,
+        yPoint: Float,
+        x0Point: Float,
+        y0Point: Float
+    ): Boolean {
+        val angel = getAngleFromTouch(
+            xPoint = xPoint,
+            yPoint = yPoint,
+            x0Point = x0Point,
+            y0Point = y0Point
+        )
+
+        return angel in startAngel..endAngel
+    }
+
+    fun isTouchInSegmentRange(
+        xPoint: Float,
+        yPoint: Float,
+        smallRadius: Float,
+        x0: Float,
+        y0: Float
+    ): Boolean {
+        val dX = xPoint - x0
+        val dY = yPoint - y0
+
+        val bigRadius = smallRadius + segmentWidth
+
+        return (dX * dX + dY * dY) < bigRadius * bigRadius
+                && (dX * dX + dY * dY) > smallRadius * smallRadius
+    }
+
+    private fun getAngleFromTouch(
+        xPoint: Float,
+        yPoint: Float,
+        x0Point: Float,
+        y0Point: Float
+    ): Float {
+        val dX = (x0Point - xPoint).toDouble()
+        val dY = (y0Point - yPoint).toDouble()
+
+        val angelInRadians = atan2(dY, dX)
+        val angelInDegrees = angelInRadians * 180 / Math.PI
+
+        return angelInDegrees.toFloat() + 180
+    }
+}
