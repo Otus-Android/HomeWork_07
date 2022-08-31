929a74f848f4e973588ca303349593ce6fd35f06
ilya <lex7555@yandex.ru>
2022-08-31 21:29:14 +0300

    Сделал анимацию для bottom bar items

--- app/src/main/java/otus/homework/customview/MainFlowFragment.kt
+++ app/src/main/java/otus/homework/customview/MainFlowFragment.kt
@@ -8,18 +8,17 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import otus.homework.customview.databinding.FragmentMainFlowBinding
-import kotlin.properties.Delegates.notNull
 
 class MainFlowFragment : Fragment() {
 
-    private var binding by notNull<FragmentMainFlowBinding>()
+    private var binding: FragmentMainFlowBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         binding = FragmentMainFlowBinding.inflate(inflater, container, false)
-        return binding.root
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -29,6 +28,11 @@ class MainFlowFragment : Fragment() {
             childFragmentManager.findFragmentById(R.id.mainFlowFragmentContainer) as NavHostFragment
         val navController = navHostFragment.navController
 
-        binding.bottomNavigationView.setupWithNavController(navController = navController)
+        binding?.bottomNavigationView?.setupWithNavController(navController = navController)
+    }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
     }
 }
