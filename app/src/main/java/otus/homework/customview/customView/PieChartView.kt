e2d2feee90dade526c5b60746f20e3d93ef4f1de
dlarin <larin992@gmail.com>
2022-11-01 16:28:05 +0300

    graph

--- app/src/main/java/otus/homework/customview/customView/PieChartView.kt
+++ app/src/main/java/otus/homework/customview/customView/PieChartView.kt
@@ -9,6 +9,7 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import otus.homework.customview.PieChartClickListener
@@ -25,6 +26,12 @@ class PieChartView @JvmOverloads constructor(
     private var mPaint: Paint = Paint()
     private var bitmap: Bitmap? = null
     private var data: List<PiePiece>? = emptyList()
+    var oval = RectF()
+    var bigOval = RectF()
+    var center_x = 0f
+    var center_y = 0f
+    var offset = 0f
+    var bOffset = 0f
 
     init {
         mPaint.color = Color.BLUE
@@ -35,6 +42,23 @@ class PieChartView @JvmOverloads constructor(
     fun setData(list: List<PiePiece>?) {
         data = list
         invalidate()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        Log.d("DDDDDD", layoutParams.width.toString())
+        Log.d("DDDDDD", layoutParams.height.toString())
+        center_x = layoutParams.height.toFloat() / 2
+        center_y = layoutParams.height.toFloat() / 2
+        offset = layoutParams.height.toFloat() / 2
+        bOffset = (layoutParams.height.toFloat() / 2) - 20f
+
+        Log.d("DDDDDD",center_x.toString())
+        Log.d("DDDDDD", center_y.toString())
+        Log.d("DDDDDD", offset.toString())
+        Log.d("DDDDDD", bOffset.toString())
+        oval[center_x - offset, center_y - offset, center_x + offset] = center_y + offset
+        bigOval[center_x - bOffset, center_y - bOffset, center_x + bOffset] = center_y + bOffset
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -51,41 +75,26 @@ class PieChartView @JvmOverloads constructor(
         super.onDraw(canvas)
         bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val newCanvas = bitmap?.let { Canvas(it) }
-        val width = 400f
-        val height = 240f
-        val radius = 100f
 
         val path = Path()
-        path.addCircle(width, height, radius, Path.Direction.CW)
+//        path.addCircle(width, height, radius, Path.Direction.CW)
         val paint = Paint()
         paint.color = Color.BLACK
         paint.isAntiAlias = true
-
-        val center_x = 240f
-        var center_y = 220f
-
-        val oval = RectF()
-        var offset = 30f
-        oval[center_x - 200f, center_y - 200f, center_x + 200f] = center_y + 200f
         data?.forEach {
             if (it.isClicked) {
-
-                oval[center_x - 220f, center_y - 220f, center_x + 220f] = center_y + 220f
+                canvas?.drawArc(oval, it.start, it.end, true, it.paint)
+                newCanvas?.drawArc(oval, it.start, it.end, true, it.paint)
             } else {
-
-                oval[center_x - 200f, center_y - 200f, center_x + 200f] = center_y + 200f
+                canvas?.drawArc(bigOval, it.start, it.end, true, it.paint)
+                newCanvas?.drawArc(bigOval, it.start, it.end, true, it.paint)
             }
-            canvas?.drawArc(oval, it.start, it.end, true, it.paint)
-            newCanvas?.drawArc(oval, it.start, it.end, true, it.paint)
 
             paint.strokeWidth = 20f
             paint.textSize = 16f
-            canvas?.drawLine(480f,0f + offset, 500f, 0f + offset, it.paint)
-            canvas?.drawText(it.category, 520f, 0f + offset, paint)
-            offset += 30f
         }
         paint.color = Color.WHITE
-        canvas?.drawCircle(center_x, center_y, radius + 60f, paint) // рисуем круг
+        canvas?.drawCircle(center_x, center_y, bOffset-40f, paint) // рисуем круг
         paint.color = Color.BLACK
         paint.textSize = 30f
         paint.textAlign = Paint.Align.CENTER
