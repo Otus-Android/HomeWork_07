3054bc156621384a51d8001657f15c6cec9f97cf
V <petrov.vladislav1992@gmail.com>
2023-03-03 17:05:45 +0600

    Sync

--- app/src/main/java/otus/homework/customview/charts/linear/LineChartView.kt
+++ app/src/main/java/otus/homework/customview/charts/linear/LineChartView.kt
@@ -1,13 +1,32 @@
 package otus.homework.customview.charts.linear
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import androidx.annotation.ColorInt
 import otus.homework.customview.charts.pie.PiePayloadEntity
 
 class LineChartView(context: Context, attributeSet: AttributeSet): View(context, attributeSet) {
 
+    private val axisPaint: Paint = Paint()
+    private val textPaint: Paint = Paint()
+    private val colors = HashMap<Int, @ColorInt Int>()
+
+    init {
+        colors[0] = Color.parseColor("#56e2cf")
+        colors[1] = Color.parseColor("#56aee2")
+        colors[2] = Color.parseColor("#5668e2")
+        colors[3] = Color.parseColor("#8a56e2")
+        colors[4] = Color.parseColor("#cf56e2")
+        colors[5] = Color.parseColor("#e256ae")
+        colors[6] = Color.parseColor("#e25668")
+        colors[7] = Color.parseColor("#e28956")
+        colors[8] = Color.parseColor("#e2cf56")
+        colors[9] = Color.parseColor("#aee256")
+    }
 
     private var payload: List<LinePayloadEntry>? = null
     fun updatePayload(payload: List<LinePayloadEntry>){
