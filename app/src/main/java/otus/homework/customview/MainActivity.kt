73351da5a9d3e19a5823bd1bcb490f0a1f5b43fc
Kamil Tayupov <k.i.tayupov@mail.ru>
2023-03-08 21:24:05 +0300

    simple animation

--- app/src/main/java/otus/homework/customview/MainActivity.kt
+++ app/src/main/java/otus/homework/customview/MainActivity.kt
@@ -2,6 +2,7 @@ package otus.homework.customview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,24 +13,31 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
 
     val items = listOf(
-      PieChartDto(1, "", 10, "", 0),
-      PieChartDto(2, "", 20, "", 0),
-      PieChartDto(3, "", 30, "", 0),
-      PieChartDto(4, "", 40, "", 0),
-      PieChartDto(5, "Simple Wine", 40, "", 0),
-      PieChartDto(6, "", 40, "", 0),
-      PieChartDto(7, "", 40, "", 0),
-      PieChartDto(8, "", 40, "", 0),
-      PieChartDto(9, "", 40, "", 0),
-      PieChartDto(10, "", 40, "", 0),
-      PieChartDto(11, "", 40, "", 0),
-      PieChartDto(12, "", 40, "", 0),
-      PieChartDto(13, "", 40, "", 0),
+      PieChartDto(1, "1", 10, "", 0),
+      PieChartDto(2, "2", 20, "", 0),
+      PieChartDto(3, "3", 30, "", 0),
+      PieChartDto(4, "4", 40, "", 0),
+      PieChartDto(5, "5", 40, "", 0),
+      PieChartDto(6, "6", 40, "", 0),
+      PieChartDto(7, "7", 40, "", 0),
+      PieChartDto(8, "8", 40, "", 0),
+      PieChartDto(9, "9", 40, "", 0),
+      PieChartDto(10, "10", 40, "", 0),
+      PieChartDto(11, "11", 40, "", 0),
+      PieChartDto(12, "12", 40, "", 0),
+      PieChartDto(13, "13", 40, "", 0),
     )
     val model = PieChartModel(
       items.map { PieChartItem(it.name, it.amount) }
     )
     chartView.model = model
-    chartView.setAccentSection(4)
+
+    var index = 0
+    thread {
+      while (true) {
+        Thread.sleep(200)
+        chartView.post { chartView.setAccentSection(++index % items.size) }
+      }
+    }
   }
 }
