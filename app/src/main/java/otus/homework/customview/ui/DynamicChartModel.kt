1a61863412da8453f4b42dffdad5cff4072757bc
Kamil Tayupov <k.i.tayupov@mail.ru>
2023-03-09 20:08:49 +0300

    store state

--- app/src/main/java/otus/homework/customview/ui/DynamicChartModel.kt
+++ app/src/main/java/otus/homework/customview/ui/DynamicChartModel.kt
@@ -1,15 +1,21 @@
 package otus.homework.customview.ui
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class DynamicChartModel(
   internal val lines: List<DynamicChartLine>
-)
+) : Parcelable
 
+@Parcelize
 data class DynamicChartLine(
   val name: String,
   val items: List<DynamicChartItem>
-)
+) : Parcelable
 
+@Parcelize
 data class DynamicChartItem(
   val amount: Int,
   val date: Long
-)
+) : Parcelable
--- app/src/main/java/otus/homework/customview/ui/DynamicChartView.kt
+++ app/src/main/java/otus/homework/customview/ui/DynamicChartView.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.View
 
@@ -52,5 +54,46 @@ class DynamicChartView @JvmOverloads constructor(
   fun updateData(model: DynamicChartModel) {
     this.model = model
     invalidate()
+  }
+
+  override fun onSaveInstanceState(): Parcelable {
+    return DynamicChartSavedState(super.onSaveInstanceState())
+  }
+
+  override fun onRestoreInstanceState(state: Parcelable?) {
+    super.onRestoreInstanceState(state)
+    if (state is DynamicChartSavedState) {
+      model = state.model
+    }
+  }
+
+  private inner class DynamicChartSavedState : BaseSavedState {
+
+    internal val model: DynamicChartModel
+
+    constructor(source: Parcelable?) : super(source) {
+      model = this@DynamicChartView.model
+    }
+
+    constructor(`in`: Parcel) : super(`in`) {
+      model = `in`.readParcelable(DynamicChartModel::class.java.classLoader) ?: DynamicChartModel(emptyList())
+    }
+
+    override fun writeToParcel(out: Parcel?, flags: Int) {
+      super.writeToParcel(out, flags)
+      out?.writeParcelable(model, 0)
+    }
+
+    @JvmField
+    val CREATE: Parcelable.Creator<DynamicChartSavedState> = object : Parcelable.Creator<DynamicChartSavedState> {
+
+      override fun createFromParcel(source: Parcel): DynamicChartSavedState {
+        return DynamicChartSavedState(source)
+      }
+
+      override fun newArray(size: Int): Array<DynamicChartSavedState?> {
+        return arrayOfNulls(size)
+      }
+    }
   }
 }
