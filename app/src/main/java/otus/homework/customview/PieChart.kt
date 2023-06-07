77c76fdd801f6fd1987c78afb371a77c7b913b95
Olga Teplitskaia <zxlnk@yandex.com>
2023-06-08 00:44:11 +0400

    legend and percent

--- app/src/main/java/otus/homework/customview/PieChart.kt
+++ app/src/main/java/otus/homework/customview/PieChart.kt
@@ -10,8 +10,11 @@ import android.util.Log
 import android.util.TypedValue
 import android.view.View
 import androidx.annotation.AttrRes
+import kotlin.math.PI
+import kotlin.math.cos
 import kotlin.math.min
 import kotlin.math.roundToInt
+import kotlin.math.sin
 
 private const val TAG = "PieChart"
 
@@ -43,36 +46,51 @@ class PieChart @JvmOverloads constructor(
         resources.displayMetrics
     )
 
-    private val paint = Paint().apply {
+    private val chartPaint = Paint().apply {
         this.color = Color.GREEN
         this.strokeWidth = thicknessPx
         this.isAntiAlias = true
         this.style = Paint.Style.STROKE
-
     }
 
     private val textPaint = Paint().apply {
         this.isAntiAlias = true
+        this.style = Paint.Style.FILL
         this.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16f, resources.displayMetrics)
     }
+    private val percentPaint = Paint(textPaint)
 
-    private var color1 = Color.RED
-    private var color2 = Color.BLUE
-    private var color3 = Color.GREEN
-    private var color4 = Color.MAGENTA
-    private var color5 = Color.CYAN
-    private var color6 = Color.YELLOW
+    private val colors = listOf<Int>(
+        Color.RED,
+        Color.BLUE,
+        Color.GREEN,
+        Color.MAGENTA,
+        Color.CYAN,
+        Color.YELLOW,
+        0xFFFF8000.toInt(),
+        0xFF00FF80.toInt(),
+        0xFF8000FF.toInt(),
+        Color.GRAY
+    )
 
-    private val colors = listOf<Int>(Color.RED, Color.BLUE, Color.GREEN, Color.MAGENTA, Color.CYAN, Color.YELLOW,
-        0xFFFFFF00.toInt(),
-        0xFF00FFFF.toInt(),
-        0xFFFF00FF.toInt(), Color.GRAY)
+    private data class Sector(
+        var angle: Float,
+        var category: String,
+        var percentText: String,
+        var percentX: Float,
+        var percentY: Float,
+        var sum: Int,
+    )
 
-    private var sectors = listOf<Triple<Float, String, String>>()
+    private var sectors = listOf<Sector>()
 
     private var selectListener: ((category: String) -> Unit)? = null
 
     private val chartRect = RectF(halfThicknessPx, halfThicknessPx, diameterPx + halfThicknessPx, diameterPx + halfThicknessPx)
+
+    private var legendLineHeight: Float
+    private var percentHalfWidth: Float
+    private var percentHalfHeight: Float
 
     init {
         // Load attributes
@@ -87,6 +105,11 @@ class PieChart @JvmOverloads constructor(
             CHART_DIAMETER + CHART_THICKNESS_DP,
             resources.displayMetrics
         ).roundToInt()
+
+        val fontMetrics = textPaint.fontMetrics
+        legendLineHeight = fontMetrics.descent - fontMetrics.ascent
+        percentHalfWidth = textPaint.measureText("100%") / 2
+        percentHalfHeight = legendLineHeight / 2
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -96,13 +119,19 @@ class PieChart @JvmOverloads constructor(
         var fullAngle = 0f
         for (i in 0..sectors.lastIndex) {
             val color = colors[i]
-            val (angle) = sectors[i]
-            paint.color = color
-            canvas.drawArc(chartRect, fullAngle, angle, false, paint)
+            val sector = sectors[i]
+            val (angle, category) = sector
+            chartPaint.color = color
+            canvas.drawArc(chartRect, fullAngle, angle, false, chartPaint)
             fullAngle += angle
+
+            canvas.drawText(sector.percentText, sector.percentX, sector.percentY, percentPaint)
+            textPaint.color = color
+            canvas.drawText(category, 0f, chartRect.bottom + halfThicknessPx + (i + 1) * legendLineHeight, textPaint)
         }
 
         canvas.drawLine(chartRect.left, chartRect.centerY(), chartRect.right, chartRect.centerY(), Paint().apply { color = Color.BLACK })
+
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -114,13 +143,13 @@ class PieChart @JvmOverloads constructor(
         val hSize = MeasureSpec.getSize(heightMeasureSpec)
 
 
-//        val width = calculateDimension(wMode, wSize)
-//        val height = calculateDimension(hMode, hSize)
+        val width = calculateDimension(wMode, wSize)
+        val height = calculateDimension(hMode, hSize)
 
-        val width = View.resolveSize(baseContentSizePx, widthMeasureSpec)
-        val height = View.resolveSize(baseContentSizePx, heightMeasureSpec)
+//        val width = View.resolveSize(baseContentSizePx, widthMeasureSpec)
+//        val height = View.resolveSize(baseContentSizePx, heightMeasureSpec)
 
-        setMeasuredDimension(width, height)
+        setMeasuredDimension(width, height + (legendLineHeight * sectors.size).roundToInt())
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -137,7 +166,7 @@ class PieChart @JvmOverloads constructor(
 
     private fun calculateDimension(mode: Int, size: Int): Int {
         return when (mode) {
-            MeasureSpec.EXACTLY,
+            MeasureSpec.EXACTLY -> size
             MeasureSpec.AT_MOST -> min(baseContentSizePx, size)
             MeasureSpec.UNSPECIFIED -> baseContentSizePx
             else -> size
@@ -153,12 +182,31 @@ class PieChart @JvmOverloads constructor(
     fun setCharges(charges: List<Charge>) {
         val categoryies = charges.groupBy { it.category }
         val sumAmount = charges.sumOf { it.amount }
-        val result: List<Triple<Float, String, String>> = charges
+        val result: List<Sector> = charges
             .groupBy { it.category }
             .mapValues { it.value.sumOf { charge -> charge.amount } }
-            .map { Triple(it.value.toFloat() * 360 / sumAmount, it.key, "${it.value * 100 / sumAmount}%") }
-            .sortedByDescending { it.first }
+            .map {
+                val radian = it.value.toDouble() * PI * 2 / sumAmount
+                Sector(
+                    it.value.toFloat() * 360 / sumAmount,
+                    it.key,
+                    "${it.value * 100 / sumAmount}%",
+                    0f,
+                    0f,
+                    it.value,
+                )
+            }
+            .sortedByDescending { it.sum }
             .take(10)
+
+        var fullAngle = 0.0
+        for (sector in result) {
+            val textHalfWidth = textPaint.measureText(sector.percentText) / 2
+            val radian = (fullAngle + sector.angle / 2) / 180.0 * PI
+            sector.percentX = (chartRect.centerX() + cos(radian) * (chartRect.width() / 2) - textHalfWidth).toFloat()
+            sector.percentY = (chartRect.centerY() + sin(radian) * (chartRect.height() / 2) + percentHalfHeight).toFloat()
+            fullAngle += sector.angle
+        }
 
         sectors = result
         invalidate()
