d6d84825bfc0e8d1948f2e65b262810c84ce5903
ivmakar <ivan.r.makarov@yandex.ru>
2023-09-09 16:12:52 +0300

    ADD -- график Pie Chart для визуализации трат по категориям

--- app/src/main/java/otus/homework/customview/MainActivity.kt
+++ app/src/main/java/otus/homework/customview/MainActivity.kt
@@ -1,11 +1,56 @@
 package otus.homework.customview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import otus.homework.customview.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var data: List<PayloadItem>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            val inputStream = resources.openRawResource(R.raw.payload)
+            val reader = BufferedReader(InputStreamReader(inputStream))
+            val json = StringBuilder()
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                json.append(line)
+            }
+            reader.close()
+
+            val gson = Gson()
+            val type = object : TypeToken<List<PayloadItem>>() {}.type
+            data = gson.fromJson(json.toString(), type)
+
+            binding.chart.setup(
+                data
+                    .groupBy { it.category }
+                    .map { (cat, amounts) ->
+                        PieChart.Category.OneCategory(cat, amounts.sumBy { it.amount }) })
+        }
+
+        binding.chart.onCategoryClickListener = object : PieChart.OnCategoryClickListener {
+            override fun onClick(category: PieChart.Category) {
+                val text = when (category) {
+                    is PieChart.Category.MultipleCategories -> "${category.names} -> ${category.value}"
+                    is PieChart.Category.OneCategory -> "${category.name} -> ${category.value}"
+                }
+                Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
 }
